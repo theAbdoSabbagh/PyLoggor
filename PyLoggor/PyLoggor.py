9a48a0677fa8aee1001848ab31608a36d91c912a
PrivatePanda <86164003+ThePrivatePanda@users.noreply.github.com>
2022-10-01 11:21:30 +0530

    v1.1.0

--- PyLoggor/PyLoggor.py
+++ PyLoggor/PyLoggor.py
@@ -21,7 +21,7 @@ class PyLoggor:
 		console_output_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "DEBUG",
 		topic_beauty_space: int = 30,
 		file_beauty_space: int = 30,
-		extras: list = [],
+		level_beauty_space: int = 10,
 		fn=False,
 		console_output=True,
 		level_colours: dict = {
@@ -30,56 +30,72 @@ class PyLoggor:
 			"WARNING": "[bold yellow]",
 			"ERROR": "[bold red]",
 			"CRITICAL": "[bold red]"
+		},
+		delim="|",
+		datefmt=r"%d-%b-%y, %H:%M:%S:%f",
+		level_symbols = {
+			"DEBUG": "D",
+			"INFO": "I",
+			"WARNING": "W",
+			"ERROR": "E",
+			"CRITICAL": "C"
 		}
 	):
 		self.file_output_level = file_output_level
 		self.console_output_level = console_output_level
 		self.topic_beauty_space = topic_beauty_space
 		self.file_beauty_space = file_beauty_space
-		self.extras = extras
+		self.level_beauty_space = level_beauty_space
 		self.file = FileHandler(fn) if fn else False
 		self.console_output = console_output
+
 		self.allowed_levels = {
 			"DEBUG": ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
 			"INFO": ["INFO", "WARNING", "ERROR", "CRITICAL"],
 			"WARNING": ["WARNING", "ERROR", "CRITICAL"],
 			"ERROR": ["ERROR", "CRITICAL"],
 			"CRITICAL": ["CRITICAL"]
-		},
+		}
+		self.level_symbols = level_symbols
+
 		self.level_colours = level_colours
+		self.delim = delim
+		self.datefmt = datefmt
 
 	def extras_builder(self, extras):
 		h = []
-		for key, value in self.extras, extras:
-				h.append(f"{key}={value}")
-		return " | ".join(h)
+		for key, value in extras.items():
+			h.append(f"{key}={value}")
+		return f" {self.delim} ".join(h)
 
-	def beautify(self, _str):
-		og_topic = _str.split(" | ")[3]
-		topic = og_topic.ljust(self.topic_beauty_space)
-		s = _str.replace(og_topic, f"{topic}")
-
-		file = s.split(" | ")[2]
-		file = file.ljust(self.file_beauty_space)
-		s = s.replace(file, f"{file}")
-
-		return s
+	def beautify(self, _str, space):
+		return _str.ljust(space)
 
 	def log(self, *,
 		level: str,
 		extras: Optional[list] = None,
-		date_fmt=r"%d-%b-%y, %H:%M:%S:%f",
 		topic="None",
 		file="NoFile",
 		msg
 	):
-		time_str = datetime.now().strftime(date_fmt)
+		level = level.upper()
+		time_str = datetime.now().strftime(self.datefmt)
 
 		if extras:
-			extras_str = f"{self.extras_builder(extras)} | "
+			extras_str = f"{self.delim} {self.extras_builder(extras)}"
 		else:
 			extras_str = ""
-		msg = self.beautify(f"{time_str} | {level}      | {file} | {topic} | {extras_str} {msg}")
+
+		beautifed_level = self.beautify(level, self.level_beauty_space)
+		beautifed_topic = self.beautify(topic, self.topic_beauty_space)
+		beautifed_file = self.beautify(file, self.file_beauty_space)
+
+		if level in self.level_symbols.keys():
+			level_symbol = self.level_symbols[level]
+		else:
+			level_symbol = "*"
+
+		msg = f"[{level_symbol}] {time_str} {self.delim} {beautifed_level} {self.delim} {beautifed_file} {self.delim} {beautifed_topic} {self.delim} {msg}{extras_str}"
 
 		if level in self.level_colours.keys():
 			level_colour = self.level_colours[level]
@@ -87,8 +103,8 @@ class PyLoggor:
 			level_colour = "[bold white]"
 
 		if self.file:
-			if level not in self.level_colours.keys() or level in self.allowed_levels[self.file_output_level]:
-				self.file.write(f"{time_str} | {topic} | {msg}")
+			if level not in self.allowed_levels.keys() or level in self.allowed_levels[self.file_output_level]:
+				self.file.write(msg)
 		if self.console_output:
-			if level not in self.level_colours.keys() or level in self.allowed_levels[self.console_output_level]:
-				print(f"{level_colour}[*]{msg}[/]")
+			if level not in self.allowed_levels.keys() or level in self.allowed_levels[self.console_output_level]:
+				print(f"{level_colour}{msg}[/]")
